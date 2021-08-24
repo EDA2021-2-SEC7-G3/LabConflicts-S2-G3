f27778a0eeecdb09a38b050bf459c0b7a951b574
José Daniel Montero Gutiérrez <j.monterog@uniandes.edu.co>
2021-08-24 18:03:54 -0500

    Est-2 4b

--- App/view.py
+++ App/view.py
@@ -86,14 +86,17 @@ while True:
 
 
         # TODO: Modificación de Est-2 en el Lab 2
-        elif int(inputs[0]) == 3:
-            print("Cargando información de Book-Tags...")
-            booktags = loadBookTags()
-            print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
+    
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
+
+    elif int(inputs[0]) == 3:
+        print("Cargando información de Book-Tags...")
+        booktags = loadBookTags()
+        print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
+        
     elif True:
         # TODO: Modificación de Est-2 en el Lab 2
         pass
